70ee4a656d2ce130ed0d731627859d13aeb2d0ef
Zakir Hossain <zakir01913@gmail.com>
2018-07-27 22:38:30 +0600

    Create User model

---
+++ app/src/main/java/com/zakir/androidtesting/persistence/User.java
@@ -0,0 +1,89 @@
+package com.zakir.androidtesting.persistence;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+
+/**
+ * Created by zakir on 26/7/18.
+ */
+@Entity(tableName = "users", indices = {@Index(value = "email", unique = true)})
+public class User {
+    @PrimaryKey
+    public int id;
+
+    @NonNull
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @NonNull
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+
+    @NonNull
+    @ColumnInfo(name = "email")
+    public String email;
+
+    @ColumnInfo(name = "designation")
+    public String designation;
+
+    @ColumnInfo(name = "company")
+    public String company;
+
+    public User(@NonNull String firstName, @NonNull String lastName, @NonNull String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
