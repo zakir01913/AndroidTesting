db1ea42792daae0e0b71a7c89fcf8b141258353a
Md. Zakir Hossain <zakir01913@gmail.com>
2018-08-01 18:17:54 +0600

    Set Observer to AddUserViewModel live data

--- app/src/androidTest/java/com/zakir/androidtesting/addUser/AddUserActivityTest.java
+++ app/src/androidTest/java/com/zakir/androidtesting/addUser/AddUserActivityTest.java
@@ -1,18 +1,31 @@
 package com.zakir.androidtesting.addUser;
 
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.zakir.androidtesting.R;
+import com.zakir.androidtesting.Response;
+import com.zakir.androidtesting.persistence.User;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -20,9 +33,13 @@ public class AddUserActivityTest {
 
     @Mock
     AddUserViewModel addUserViewModel;
+    @Mock
+    MutableLiveData<Response<User>> responseMutableLiveData;
+    @Captor
+    ArgumentCaptor<Observer<Response<User>>> observerArgumentCaptor;
 
     @Rule
-    ActivityTestRule<AddUserActivity> addUserActivityActivityTestRule = new ActivityTestRule<>(
+    public ActivityTestRule<AddUserActivity> addUserActivityActivityTestRule = new ActivityTestRule<>(
             AddUserActivity.class
     );
 
@@ -30,10 +47,21 @@ public class AddUserActivityTest {
     public void setup() {
         MockitoAnnotations.initMocks(this);
         addUserActivityActivityTestRule.getActivity().addUserViewModel = addUserViewModel;
+        when(addUserViewModel.response()).thenReturn(responseMutableLiveData);
+        addUserActivityActivityTestRule.getActivity().setAddUserViewModel(addUserViewModel);
+        verify(responseMutableLiveData).observe(isA(LifecycleOwner.class), observerArgumentCaptor.capture());
     }
 
     @Test
-    public void save_withInvalidFirstName_showErrorInFirstNameEditText() {
+    public void save_callAddUserViewModelInsert() {
+
+        onView(ViewMatchers.withId(R.id.save_btn)).perform(click());
+
+        verify(addUserViewModel).insert(null);
+    }
+
+    @Test
+    public void save_withEmptyFirstName_showError() {
 
     }
 }
--- app/src/main/java/com/zakir/androidtesting/addUser/AddUserActivity.java
+++ app/src/main/java/com/zakir/androidtesting/addUser/AddUserActivity.java
@@ -1,17 +1,38 @@
 package com.zakir.androidtesting.addUser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.zakir.androidtesting.R;
+import com.zakir.androidtesting.Response;
+import com.zakir.androidtesting.persistence.User;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddUserActivity extends AppCompatActivity {
 
     AddUserViewModel addUserViewModel;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+        ButterKnife.bind(this);
+    }
+
+    public void setAddUserViewModel(AddUserViewModel addUserViewModel) {
+        this.addUserViewModel = addUserViewModel;
+        this.addUserViewModel.response().observe(this, observer -> handleResponse(observer));
+    }
+
+    @OnClick(R.id.save_btn)
+    public void save() {
+        addUserViewModel.insert(user);
+    }
+
+    private void handleResponse(Response<User> responseObserver) {
+
     }
 }
